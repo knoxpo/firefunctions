474763f50425157f63b6566d2e155ae8a9c22584
nhathiwala <nanx.racer@gmail.com>
2017-08-11 14:43:55 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,2 @@
 export const environment = {
-      firebaseConfig: {
-            apiKey: 'AIzaSyBy9lBB0FF6rd9m_LAfQlXzGvdUWtsXqf8',
-            authDomain: 'knoxpo-60263.firebaseapp.com',
-            databaseURL: 'https://knoxpo-60263.firebaseio.com',
-            projectId: 'knoxpo-60263',
-            storageBucket: 'knoxpo-60263.appspot.com',
-            messagingSenderId: '33648901994'
-      }
 };
